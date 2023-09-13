3b1bf75018cf026614264748d8b5734909c16525
AquaFantom <herobrine200403@gmail.com>
2023-09-13 22:01:21 +0300

    Meow

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        String user_name = "Anna";
-        System.out.printf("Your name is " + user_name);
+        System.out.printf("meow meow meow");
     }
 }
